9c71965c63f6566cde17ff8f971c8392aa5f28d9
Oguz Gelal <oguz.gelal@vub.ac.be>
2018-05-06 13:38:34 +0200

    move react middleware into its own repo

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import commonjs from 'rollup-plugin-commonjs';
+import uglify from 'rollup-plugin-uglify';
+
+const env = process.env.NODE_ENV;
+const ext = env === 'production' ? 'min.js' : 'js';
+
+const config = {
+  external: ['react', 'reclare'],
+  input: 'src/index.js',
+  output: {
+    name: 'React Reclare',
+    file: `dist/react-reclare.${ext}`,
+    format: 'umd',
+    globals: {
+      react: 'React',
+      reclare: 'Reclare',
+    },
+  },
+  plugins: [
+    nodeResolve(),
+    babel({ exclude: '**/node_modules/**' }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify(env) }),
+    commonjs()
+  ],
+};
+
+if (env === 'production') {
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+        warnings: false
+      }
+    })
+  );
+}
+
+export default config;
---
+++ src/provider/providerFactory.js
@@ -0,0 +1,38 @@
+import React, { PropTypes } from 'react';
+
+import { Provider } from '../context';
+import initialize from '../initialize';
+
+let provider = null;
+
+class Reclare extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    provider = this;
+
+    this.state = {};
+  }
+
+  componentWillMount() {
+    const { declarations, initialState } = this.props;
+
+    // initialize the reclare & react-reclare
+    initialize({ provider, declarations, initialState });
+
+    // set the for the first load
+    this.setState({ value: initialState });
+  }
+
+  render() {
+    return <Provider value={this.state.value}>{this.props.children}</Provider>;
+  }
+}
+
+Reclare.propTypes = {
+  declarations: PropTypes.array,
+  initialState: PropTypes.object
+};
+
+export { provider };
+export default Reclare;
